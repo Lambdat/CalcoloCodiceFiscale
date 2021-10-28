dd2f0dc1d43e990fd0160d005df2107b7c1a0443
Lambdat <oreficeantonio98@postecert.it>
2021-10-28 09:59:24 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcoloCodiceFiscale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User("antonio", "orefice", DateTime.Parse("1998-02-24"), "uomo", "napoli");
+
+            //Console.WriteLine(u.CreateSsn());
+
+            u.CreateSsn(); //Richiamo il metodo void per effettuare il calcolo e caricare la proprietà Ssn
+
+            Console.WriteLine(u.Ssn);
+        }
+    }
+}
